7ef57e8dd89db0d2f0fda7d77d166ad343736c66
IsaPhipho <isaphiladelpho@gmail>
2019-10-18 22:49:18 +0100

    chore: Altera retorno da rota

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get('/teste', (req, res)=>{
-  return res.json({heloo: 'World'})
+  return res.json({heloo: 'World2'})
 });
 
 app.listen(3333);
